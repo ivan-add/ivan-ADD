df308f588c2ce04de265c562d981bc940cf356e6
root <ivandiaz>
2025-09-22 16:28:17 +0100

    SCript 4 y 5

--- ivanbash.sh
+++ ivanbash.sh
@@ -8,24 +8,31 @@ echo "5. contar"
 echo "6. permisosoctal"
 echo "7. romano"
 echo "0. salida"
-read -p "Elige una opción" op
+read -p "Elige una opción " op
 echo ""
 }
 menu
-case $op == 0 in
+case $op in
 	0)
 	echo "Has salido del script"
 	;;
-	3)
-	num=$(( (RANDOM % 100) + 1))
-	tries=0
-	while [[ $tries -le 5  || $n -ne num ]]; do
-     	   read -p "Di un numero aletorio del 1 al 100" n
-	   intentos++
-	   if [ $n -gt $num ]; then
-	      echo "El numero Aleatoriamente puesto es menor a $n"
-	      echo "Este es tu intento numero $intentos"
-	   fi
-
+	4)
+	read -p "Escriba el nombre de un fichero " f
+	busqueda=$(find / -type f -name "$f" 2>/dev/null)
+	if [ -f "$busqueda" ]; then
+ 	    vocales=$(grep -o -i "[aeiouáéíóú]" $f | wc -l)
+   	    echo "El archivo se encuentra en $busqueda y el archivo tiene un total de $vocales vocales"
+	else
+	    echo "No Existe el fichero $f"
+	fi
+	;;
+	5)
+	read -p "Contar cuantos ficheros hay en un directorio " d
+	if [ -d "$d" ]; then
+	   contar=$(find $d -type f | wc -l 2>/dev/null)
+	   echo "Hay un total de $contar ficheros en el directorio $d"
+	else
+	   echo "El directorio $d no exite"
+	fi
 	;;
 esac
